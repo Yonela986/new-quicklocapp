605dda3a2436ae9a0c3f3b62305a00ae067313b1
Yonela <yoza.futho@gmail.com>
2021-11-16 11:26:52 +0200

    quickloc app added

--- src/components/GoogleMap.vue
+++ src/components/GoogleMap.vue
@@ -35,54 +35,92 @@
 
 <script>
 
-import data from '../assets/messages.json';
+import { maplocation } from '../assets/messages.json';
 export default {
   name: 'GoogleMap',
   data() {
     return {
         center:{Lat:51.508742,lng:-0.120850},
-                zoom:5,  
-                currentPlace: null,
-      markers: [],
-      places: [],
-        }
+                infoContent: '',
+      infoWindowPos: null,
+      infoWinOpen: false,
+      currentMidx: null,
+      infoOptions: {
+          pixelOffset: {
+          width: 0,
+          height: -35
+          }
+        },
+    };
   },
-  computed: {
-    google: gmapApi,
-    mapData() {
-      return JSON.parse(data);
-    },
-   mounted() {
-    this.geolocate();
-  },
-  methods: {
-    setPlace(place) {
-      this.currentPlace = place;
-    },
-    addMarker() {
-      if (this.currentPlace) {
-        const marker = {
-          lat: this.currentPlace.geometry.location.lat(),
-          lng: this.currentPlace.geometry.location.lng(),
-        };
-        this.markers.push({ position: marker });
-        this.places.push(this.currentPlace);
-        this.center = marker;
-        this.currentPlace = null;
+  computed:{
+      markers(){
+          return maplocation.map(({location: { lat, lng}, heading }) => ({
+              position: {
+                  lat,
+                  lng
+              },
+              heading
+          }));
       }
-    },
-  methods: {
-    geolocate: function() {
-      navigator.geolocation.getCurrentPosition(position => {
-        this.center = {
-          lat: position.coords.latitude,
-          lng: position.coords.longitude,
-        };
-      });
-    },
   },
+  methods: {
+      getPosition : function(marker){
+          return {
+              lat: parseFloat(marker.position.lat),
+              lng: parseFloat(marker.position.lng)
+          }
+      },
+        toggleInfoWindow: function(marker, index) {
+      this.infoWindowPos = this.getPosition(marker);
+      this.infoContent = marker.text;
+
+      if (this.currentMidx == index) {
+        this.infoWinOpen = !this.infoWinOpen;
+      }
+      else{
+        this.infoWinOpen = true;
+        this.currentMidx = index;
+      }
+    }
   }
-  }
-}
+};
+  
+
+//   computed: {
+//     google: gmapApi,
+//     mapData() {
+//       return JSON.parse(data);
+//     },
+//    mounted() {
+//     this.geolocate();
+//   },
+//   methods: {
+//     setPlace(place) {
+//       this.currentPlace = place;
+//     },
+    // addMarker() {
+    //   if (this.currentPlace) {
+    //     const marker = {
+    //       lat: this.currentPlace.geometry.location.lat(),
+    //       lng: this.currentPlace.geometry.location.lng(),
+    //     };
+    //     this.markers.push({ position: marker });
+    //     this.places.push(this.currentPlace);
+    //     this.center = marker;
+    //     this.currentPlace = null;
+    //   }
+    // },
+//   methods: {
+//     geolocate: function() {
+//       navigator.geolocation.getCurrentPosition(position => {
+//         this.center = {
+//           lat: position.coords.latitude,
+//           lng: position.coords.longitude,
+//         };
+//       });
+//     },
+//   },
+ 
 </script>
 
